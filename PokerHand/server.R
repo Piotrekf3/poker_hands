174a0c10de5b4529e7699ad623c3ce93fff946c3
Piotrekf3 <piotrek.fur@wp.pl>
2019-12-11 22:13:46 +0100

    Finish pie chart

--- PokerHand/server.R
+++ PokerHand/server.R
@@ -11,25 +11,39 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
+getValue <- function(keys, values) {
+    result <- c()
+    keys <- paste(keys)
+    for(value in values) {
+        if(any(startsWith(value, keys))) {
+            result <- append(result, value)
+        }
+    }
+    return(result)
+}
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     mydata = read.csv("training.data")
     colnames(mydata) <- c("S1", "C1", "S2", "C2", "S3", "C3", "S4", "C4", "S5", "C5", "CLASS")
     
     class_probability = reactive ({ mydata %>%
-        filter(CLASS %in% input$classes) %>%
         group_by(CLASS) %>%
         summarise(n = n()) %>%
-        mutate(freq = round(n / sum(n), 6), CLASS = as.factor(c("0: Nothing in hand; not a recognized poker hand",
-                                                                "1: One pair; one pair of equal ranks within five cards",
-                                                                "2: Two pairs; two pairs of equal ranks within five cards",
-                                                                "3: Three of a kind; three equal ranks within five cards",
-                                                                "4: Straight; five cards, sequentially ranked with no gaps",
-                                                                "5: Flush; five cards with the same suit",
-                                                                "6: Full house; pair + different rank three of a kind",
-                                                                "7: Four of a kind; four equal ranks within five cards",
-                                                                "8: Straight flush; straight + flush",
-                                                                "9: Royal flush; {Ace, King, Queen, Jack, Ten} + flush")))})
+        mutate(freq = round(n / sum(n), 6))%>%
+        filter(CLASS %in% input$classes) %>%
+        mutate(CLASS = as.factor(getValue(input$classes, c("0: Nothing in hand; not a recognized poker hand",
+                                   "1: One pair; one pair of equal ranks within five cards",
+                                   "2: Two pairs; two pairs of equal ranks within five cards",
+                                   "3: Three of a kind; three equal ranks within five cards",
+                                   "4: Straight; five cards, sequentially ranked with no gaps",
+                                   "5: Flush; five cards with the same suit",
+                                   "6: Full house; pair + different rank three of a kind",
+                                   "7: Four of a kind; four equal ranks within five cards",
+                                   "8: Straight flush; straight + flush",
+                                   "9: Royal flush; {Ace, King, Queen, Jack, Ten} + flush"))))    
+        }) 
+    
 
     
     output$classProbability <- renderPlot({
--- PokerHand/ui.R
+++ PokerHand/ui.R
@@ -43,7 +43,11 @@ shinyUI(dashboardPage(
                                                         "9: Royal flush; {Ace, King, Queen, Jack, Ten} + flush"),
                                                       choiceValues = c(
                                                           0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-                                                      )))
+                                                      ),
+                                                      selected = c(
+                                                          0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+                                                      )
+                                            ))
                     )
             )
         )
